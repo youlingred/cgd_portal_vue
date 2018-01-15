c296465e815853a431912ee8519780d662ab04f8
xieyanhui <30897863@qq.com>
2018-01-15 20:29:47 +0800

    添加Tabs实例

--- src/pages/test/tabs.vue
+++ src/pages/test/tabs.vue
@@ -1,7 +1,26 @@
 <template>
   <el-card>
     <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="待处理" name="1">待处理</el-tab-pane>
+      <el-tab-pane label="待处理" name="1">
+        <el-form ref="form" :model="form" :inline="true">
+          <el-form-item label="计划名称：" prop="name">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="项目单位：" prop="project">
+            <el-input v-model="form.project"></el-input>
+          </el-form-item>
+          <el-form-item label="受理状态" prop="status">
+            <el-select v-model="form.status" placeholder="请选择">
+              <el-option v-for="item in status_options" :key="item.value" :label="item.label"
+                         :value="item.value"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="search">搜索</el-button>
+            <el-button @click="resetForm('form')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
       <el-tab-pane label="已处理" name="2">已处理</el-tab-pane>
     </el-tabs>
   </el-card>
@@ -10,10 +29,37 @@
   export default {
     data() {
       return {
-        activeName: '2'
+        activeName: '2',
+        //受理状态
+        status_options: [
+          {
+            label: '受理中',
+            value: 1
+          },
+          {
+            label: '已处理',
+            value: 2
+          },
+          {
+            label: '驳回',
+            value: 3
+          },
+        ],
+        //表单数据
+        form: {
+          name: '',
+          project: '',
+          status: '',
+        },
       };
     },
     methods: {
+      search() {
+      },
+      //重置
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
       handleClick(tab) {
         console.log(tab);
       }
