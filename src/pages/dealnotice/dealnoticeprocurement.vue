d613dc1396d261ad364c389136973506e2338340
guohp <1336321395@qq.com>
2018-01-19 10:28:09 +0800

    添加

--- src/pages/dealnotice/dealnoticeprocurement.vue
+++ src/pages/dealnotice/dealnoticeprocurement.vue
@@ -2,99 +2,120 @@
   <el-card>
     <el-tabs v-model="activeName" @tab-click="handleClick">
       <el-tab-pane label="待处理" name="1">
-        <el-form ref="form1" :model="form" :inline="true">
-          <el-form-item label="状态:" prop="status">
-            <el-select v-model="form.status" placeholder="请选择">
+        <el-form ref="form1" :model="form1" :inline="true">
+          <el-form-item   label="状态:" prop="status">
+            <el-select v-model=" form1.status" placeholder="请选择">
               <el-option v-for="item in status_options" :key="item.value" :label="item.label"
                          :value="item.value"></el-option>
             </el-select>
-          </el-form-item>
-          <el-form-item label="成交通知书编号:" prop="numbering">
-            <el-input v-model="form.numbering"></el-input>
-          </el-form-item>
-          <el-form-item label="成交通知书名称" prop="name">
-            <el-input v-model="form.name"></el-input>
-          </el-form-item>
-          <el-form-item label="采购类别:" prop="ctype">
-            <el-select v-model="form.ctype" placeholder="请选择">
+          </el-form-item  >
+          <el-form-item   label="成交通知书编号:" prop="numbering">
+            <el-input v-model=" form1.numbering"></el-input>
+          </el-form-item  >
+          <el-form-item   label="成交通知书名称" prop="name">
+            <el-input v-model=" form1.name"></el-input>
+          </el-form-item  >
+          <el-form-item   label="采购类别:" prop="ctype">
+            <el-select v-model=" form1.ctype" placeholder="请选择">
               <el-option v-for="item in ctype_options" :key="item.value" :label="item.label"
                          :value="item.value"></el-option>
             </el-select>
-          </el-form-item>
-          <el-form-item label="供应商:" prop="supplier">
-            <el-input v-model="form.supplier"></el-input>
-          </el-form-item>
-          <el-form-item label="缴费通知发送状态" prop="pstatus">
-            <el-select v-model="form.pstatus" placeholder="请选择">
+          </el-form-item  >
+          <el-form-item   label="供应商:" prop="supplier">
+            <el-input v-model=" form1.supplier"></el-input>
+          </el-form-item  >
+          <el-form-item   label="缴费通知发送状态:" prop="pstatus">
+            <el-select v-model=" form1.pstatus" placeholder="请选择">
               <el-option v-for="item in pstatus_options" :key="item.value" :label="item.label"
                          :value="item.value"></el-option>
             </el-select>
-          </el-form-item>
-          <div ref="hideSearch1" v-show="displaySearch">
-            <el-form-item label="活动时间" prop="timesegment" required >
-              <el-date-picker
-                v-model="form.timesegment"
-                type="daterange"
-                start-placeholder="开始日期"
-                end-placeholder="结束日期"
-                :default-value="inittime">
-              </el-date-picker>
-            </el-form-item>
+          </el-form-item  >
+          <div v-show="displaySearch1">
+            <el-form-item   label="制单日期:"  >
+              <el-col :span="10">
+                <el-date-picker
+                  v-model=" form1.timesegmentstart"
+                  :editable="editable"
+                  type="datetime"
+                  placeholder="选择日期时间">
+                </el-date-picker>
+              </el-col>
+              <el-col class="line" :span="2" style="width:40px;margin-left:38px">&oline;</el-col>
+              <el-col :span="10">
+                <el-date-picker
+                  v-model=" form1.timesegmentend"
+                  :editable="editable"
+                  type="datetime"
+                  placeholder="选择日期时间">
+                </el-date-picker>
+              </el-col>
+            </el-form-item  >
           </div>
-          <el-form-item>
+          <el-form-item  >
             <el-button type="primary" @click="search">搜索</el-button>
-            <el-button @click="resetForm('form1')">重置</el-button>
+            <el-button @click="resetform('form11')">重置</el-button>
             <!--第二种展开收齐方式:传参-->
-            <button-open-close :el="$refs.hideSearch1" :open="displaySearch"></button-open-close>
-          </el-form-item>
+            <button-open-close @change="toggle" :open="displaySearch1"></button-open-close>
+          </el-form-item  >
         </el-form>
       </el-tab-pane>
       <el-tab-pane label="已处理" name="2">
-        <el-form ref="form2" :model="form" :inline="true">
-          <el-form-item label="状态:" prop="status">
-            <el-select v-model="form.status" placeholder="请选择">
+        <el-form ref="form2" :model="form2" :inline="true">
+          <el-form-item   label="状态:" prop="status">
+            <el-select v-model="form2.status" placeholder="请选择">
               <el-option v-for="item in status_options" :key="item.value" :label="item.label"
                          :value="item.value"></el-option>
             </el-select>
-          </el-form-item>
-          <el-form-item label="成交通知书编号:" prop="numbering">
-            <el-input v-model="form.numbering"></el-input>
-          </el-form-item>
-          <el-form-item label="成交通知书名称:" prop="name">
-            <el-input v-model="form.name"></el-input>
-          </el-form-item>
-          <el-form-item label="采购类别:" prop="ctype">
-            <el-select v-model="form.ctype" placeholder="请选择">
+          </el-form-item  >
+          <el-form-item   label="成交通知书编号:" prop="numbering">
+            <el-input v-model="form2.numbering"></el-input>
+          </el-form-item  >
+          <el-form-item   label="成交通知书名称:" prop="name">
+            <el-input v-model="form2.name"></el-input>
+          </el-form-item  >
+          <el-form-item   label="采购类别:" prop="ctype">
+            <el-select v-model="form2.ctype" placeholder="请选择">
               <el-option v-for="item in ctype_options" :key="item.value" :label="item.label"
                          :value="item.value"></el-option>
             </el-select>
-          </el-form-item>
-          <el-form-item label="供应商:" prop="supplier">
-            <el-input v-model="form.supplier"></el-input>
-          </el-form-item>
-          <el-form-item label="缴费通知发送状态:" prop="pstatus">
-            <el-select v-model="form.pstatus" placeholder="请选择">
+          </el-form-item  >
+          <el-form-item   label="供应商:" prop="supplier">
+            <el-input v-model="form2.supplier"></el-input>
+          </el-form-item  >
+          <el-form-item   label="缴费通知发送状态:" prop="pstatus">
+            <el-select v-model="form2.pstatus" placeholder="请选择">
               <el-option v-for="item in pstatus_options" :key="item.value" :label="item.label"
                          :value="item.value"></el-option>
             </el-select>
-          </el-form-item>
-          <div ref="hideSearch2" v-show="displaySearch">
-            <el-form-item label="活动时间:" required >
-              <el-date-picker
-                v-model="form.timesegment"
-                type="daterange"
-                start-placeholder="开始日期"
-                end-placeholder="结束日期"
-                :default-value="inittime">
-              </el-date-picker>
-            </el-form-item>
+          </el-form-item  >
+          <div v-show="displaySearch2">
+            <el-form-item   label="制单日期:" >
+              <el-col :span="10">
+                <el-date-picker
+                  v-model="form2.timesegmentstart"
+                  :editable="editable"
+                  type="datetime"
+                  placeholder="选择日期时间">
+                </el-date-picker>
+              </el-col>
+              <el-col class="line" :span="2" style="width:40px;margin-left:38px">&oline;</el-col>
+              <el-col :span="10">
+                <el-date-picker
+                  v-model="form2.timesegmentend"
+                  :editable="editable"
+                  type="datetime"
+                  <!--@change=""  moment('2010-10-20').isBefore('2010-10-21'); // true-->
+                  placeholder="选择日期时间">
+                </el-date-picker>
+              </el-col>
+            </el-form-item  >
           </div>
-          <el-form-item>
+          <el-form-item  >
             <el-button type="primary" @click="search">搜索</el-button>
-            <el-button @click="resetForm('form2')">重置</el-button>
+            <el-button @click="resetform('form12')">重置</el-button>
             <!--第二种展开收齐方式:传参-->
-            <button-open-close :el="$refs.hideSearch2" :open="displaySearch"></button-open-close>
-          </el-form-item>
+            <button-open-close @change="toggle2" :open="displaySearch2"></button-open-close>
+          </el-form-item  >
         </el-form>
       </el-tab-pane>
     </el-tabs>
@@ -109,8 +130,10 @@
     },
     data() {
       return {
-        inittime: this.moment().subtract(5, "months").format("YYYY-MM-DD"),
-        displaySearch:false,
+        editable:false,
+        // from2start: '2018-01-19', //this.moment().subtract(5, "months").format("YYYY-MM-DD HH:mm:ss"),
+        displaySearch1:false,
+        displaySearch2:false,
         activeName: '1',
         //受理状态
         status_options: [
@@ -168,14 +191,25 @@
           },
         ],
         //表单数据
-        form: {
+         form1: {
           status: '',
           numbering: '',
           name: '',
           ctype:'',
           supplier:'',
           pstatus:'',
-          timesegment:''
+          timesegmentstart:'',
+          timesegmentend:''
+        },
+        form2: {
+          status: '',
+          numbering: '',
+          name: '',
+          ctype:'',
+          supplier:'',
+          pstatus:'',
+          timesegmentstart:'',
+          timesegmentend:''
         },
       };
     },
@@ -184,11 +218,17 @@
 
       },
       //重置
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
+      resetform(form1Name) {
+        this.$refs[form1Name].resetFields();
       },
       handleClick(tab) {
         console.log(tab);
+      },
+      toggle(open){
+        this.displaySearch1=open;
+      },
+      toggle2(open) {
+        this.displaySearch2=open;
       }
     }
   };
