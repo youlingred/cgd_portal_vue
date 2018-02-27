4764bfac93725298ed272a61cf0486c801cd8bde
Zhou <363420113@qq.com>
2018-02-27 11:00:02 +0800

    添加路由配置base:'/dist/'

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -24,7 +24,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
-    publicPath: "/dist",
+    publicPath: "/dist/",
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
